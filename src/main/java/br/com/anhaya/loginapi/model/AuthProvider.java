b60321823d48dfe31e9545fee937fca3b9cb66aa
Carlos Andre Anhaya de Barros <candre@cit>
2019-04-08 16:46:42 -0300

    first commit

---
+++ src/main/java/br/com/anhaya/loginapi/model/AuthProvider.java
@@ -0,0 +1,6 @@
+package br.com.anhaya.loginapi.model;
+
+public enum  AuthProvider {
+    facebook,
+    google
+}
---
+++ src/main/java/br/com/anhaya/loginapi/security/CurrentUser.java
@@ -0,0 +1,13 @@
+package br.com.anhaya.loginapi.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
